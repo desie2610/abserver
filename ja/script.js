6ea46ea89033ddd145ff8ce82d7af35407cbe829
desie2610 <moiseenkodanil2013@gmail.com>
2025-10-14 14:04:52 +0300

    аfff

---
+++ ja/script.js
@@ -0,0 +1,17 @@
+
+const images = document.querySelectorAll('img[data-src]');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.getAttribute('data-src'); 
+      observer.unobserve(img);
+    }
+  });
+});
+
+
+images.forEach(img => {
+  observer.observe(img);
+});
